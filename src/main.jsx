4244c77809fcc53312c555748b181b50721beab0
Muse Mulatu <musemulatu.g@gmail.com>
2026-06-02 19:44:56 +0300

    routes, tailwind fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+// src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 1. Import BrowserRouter
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 2. Wrap App inside BrowserRouter */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
